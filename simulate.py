d8ade3eeda28b162e091be8d5a83830f19a8639a
Julie Dieroff <s2175689@ed.ac.uk>
2024-10-26 22:49:46 +0100

    Made the boundary condition optional
    Added a 'switch' to turn off the Newton cooling boundary condition and make is Neuman

--- simulate.py
+++ simulate.py
@@ -49,6 +49,39 @@ def simulate_dirichlet_sink(u0, b0, D, T, Nt_points, L, Nx_points, a, u_inf = 0)
 
     return U
 
+def create_analytic_toy_fn(b0, bL, L, D, n_sum_terms = 200):
+    """Create a function that returns the value of the analytic solution to the toy problem with the given parameters."""
+    
+    def U_analyt(x, t):
+        """x can be a 1-d array"""
+        if t == 0: # initial condition
+            sol = bL * np.ones(len(x))
+            sol[0] = b0
+        else:
+            u_d = b0 - bL
+
+            sol = b0 - u_d*(x / L)
+            for n in range(1,n_sum_terms):
+                sol -= 2*u_d/(n*np.pi) * np.sin(n*np.pi*x/L) * np.exp(-(n*np.pi/L)**2 * D * t)
+
+        return sol
+    
+    return U_analyt
+
+def create_analytic_avg_temp(b0, bL, L, D, n_summation = 200):
+    """Returns the analytic function describing the average temperature at times t"""
+    def U_avg_analyt(t):
+        """t is in seconds. Must be an np.array"""
+        u_d = b0 - bL
+        sol = b0 - u_d/2
+
+        for k in range(n_summation):
+            sol -= 4*u_d/np.pi**2 * np.exp(-((2*k + 1)*np.pi/L)**2 * D * t) / (2*k + 1)**2
+
+        return sol
+
+    return U_avg_analyt
+
 def create_numeric_avtemp_fn(U, dx, time_to_index):
     """Returns the numeric function describing the average temperature at times t"""
     def avg_temp_numeric(t):
@@ -69,7 +102,7 @@ def time_to_temp(avg_temps, temp, index_to_time):
     print(f"None of the given average temperatures is above {temp}.")
     return None
 
-def simulate_sink_with_fancy_bcs(u0, b0, D, T, Nt_points, L, Nx_points, a, h, kW, u_inf = 0):
+def simulate_sink_with_fancy_bcs(u0, b0, D, T, Nt_points, L, Nx_points, a, h, kW, u_inf = 0, insulated = False):
     """
     Simulates the heat distribution over time of a material over time with one Newton cooling boundary condition, one endpoint with a time dependent temperature, and a sink term, using a backward Euler scheme.
     
@@ -84,7 +117,8 @@ def simulate_sink_with_fancy_bcs(u0, b0, D, T, Nt_points, L, Nx_points, a, h, kW
         a (float): The value of the heat transfer coefficent for the sink term
         h (float): The value of the heat transfer coefficent for the top boundary condition
         kW (float): The thermal conductivity of the liquid within the can.
-        u_inf (float): The ambient (air) temperature, for the sink term and fancy boundary conditions.        
+        u_inf (float): The ambient (air) temperature, for the sink term and fancy boundary conditions.
+        insulated (bool): If true then the Newton cooling boundary condition is off and the top boundary condition is Neuman boundary condition =0
     Returns:
         U (matrix): A Nx_points by Nt_points matrix, where each column is the simulated heat distribution at a time t.
 
@@ -106,14 +140,20 @@ def simulate_sink_with_fancy_bcs(u0, b0, D, T, Nt_points, L, Nx_points, a, h, kW
     # implement the time dependent bottom boundary condition
     A[0,0] = 1
 
-    # Implement the Newton cooling boundary condition at the top
-    A[Nx_points-1,Nx_points-1] = 1+2*C + (C*2*dx*h)/kW
-    A[Nx_points-1,Nx_points-2] = -2*C
+    if insulated == False:
+        # Implement the Newton cooling boundary condition at the top
+        A[Nx_points-1,Nx_points-1] = 1+2*C + (C*2*dx*h)/kW
+        A[Nx_points-1,Nx_points-2] = -2*C
 
-    # Create the vector that updates u_old to account for the Newton cooling boundary condition
-    newtcool = np.zeros(Nx_points)
-    # Set up the rest of the Newton cooling boundary condition
-    newtcool[-1] = (C*2*dx*h*u_inf)/kW
+        # Create the vector that updates u_old to account for the Newton cooling boundary condition
+        newtcool = np.zeros(Nx_points)
+        # Set up the rest of the Newton cooling boundary condition
+        newtcool[-1] = (C*2*dx*h*u_inf)/kW
+
+    else: 
+        newtcool = 0
+        A[Nx_points-1,Nx_points-1] = 1+2*C 
+        A[Nx_points-1,Nx_points-2] = -2*C
 
     # Run simulation
     for n in range(1, Nt_points):
